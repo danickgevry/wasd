3b584951af6590b77002765df2eb0fe19affdfb9
Danick Gevry <danickgevry@gmail.com>
2023-04-21 18:41:20 -0400

    Import initial draft of the app

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import * as bootstrap from 'bootstrap'
 
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
+import 'bootstrap-icons/font/bootstrap-icons.scss'
+import './assets/main.scss'
 
 const app = createApp(App)
 
